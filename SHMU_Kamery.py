b59db1d9826e85111ce91fddb308ebc80881d993
AshSidney <ashsidney@gmail.com>
2019-02-07 22:15:24 +0100

    ziskanie zoznamu obrazkov z kamery

--- SHMU_Kamery.py
+++ SHMU_Kamery.py
@@ -1,11 +1,25 @@
 import urllib.request
 import html.parser
+import re
 
 def dajZoznamKamier():
-  kamery = KameraParser()
+  parser = KameraParser()
   with urllib.request.urlopen(shmuAdresa + '?page=1&id=webkamery') as stranka:
-    kamery.feed(stranka.read().decode('utf-8'))
-  return kamery.kamery
+    parser.feed(stranka.read().decode('utf-8'))
+  return parser.kamery
+
+def dajObrazkyKamery(kameraLink):
+  parser = ObrazkyParser()
+  with urllib.request.urlopen(shmuAdresa + kameraLink + '&c=360') as stranka:
+    parser.feed(stranka.read().decode('utf-8'))
+  return parser.obrazky
+
+
+def dajAtribut(attrs, attrNazov):
+  for attr in attrs:
+    if attr[0] == attrNazov:
+      return attr[1]
+  return None
 
 
 class KameraParser (html.parser.HTMLParser):
@@ -16,21 +30,15 @@ class KameraParser (html.parser.HTMLParser):
     self.aktualnaKamera = None
     self.vNazveKamery = False
 
-  def dajAtribut(self, attrs, attrNazov):
-    for attr in attrs:
-      if attr[0] == attrNazov:
-        return attr[1]
-    return None
-
   def handle_starttag(self, tag, attrs):
     if self.kameraDivPocet == 0:
-      if tag == 'div' and self.dajAtribut(attrs, 'id') == 'maincontent':
+      if tag == 'div' and dajAtribut(attrs, 'id') == 'maincontent':
         self.kameraDivPocet = 1
     else:
       if tag == 'div':
         self.kameraDivPocet += 1
       elif tag == 'a':
-        self.aktualnaKamera = self.dajAtribut(attrs, 'href')
+        self.aktualnaKamera = dajAtribut(attrs, 'href')
       elif tag == 'h3':
         self.vNazveKamery = True
 
@@ -43,6 +51,33 @@ class KameraParser (html.parser.HTMLParser):
       self.kamery.append((self.aktualnaKamera, data))
       self.aktualnaKamera = None
       self.vNazveKamery = False
+
+
+class ObrazkyParser (html.parser.HTMLParser):
+  def __init__(self):
+    super(ObrazkyParser, self).__init__()
+    self.obrazky = []
+    self.vSkripte = False
+
+  def handle_starttag(self, tag, attrs):
+    if tag == 'script' and dajAtribut(attrs, 'type') == 'text/javascript':
+      self.vSkripte = True
+
+  def handle_endtag(self, tag):
+    if self.vSkripte and tag == 'script':
+      self.vSkripte = False
+
+  def handle_data(self, data):
+    if self.vSkripte and re.match(r'\s*var img_dts.*', data):
+      vzor = re.compile(r'img_dts\[\d*\]=\'(.*?)\'; img_files\[\d*\]=\'(.*?)\';')
+      start = 0
+      while start >= 0:
+        najdene = vzor.search(data, start)
+        if najdene is not None:
+          self.obrazky.append((najdene.group(2), najdene.group(1)))
+          start = najdene.span()[1]
+        else:
+          start = -1
 
 
 shmuAdresa = 'http://www.shmu.sk/sk/'
--- kameryZoznamTest.py
+++ kameryZoznamTest.py
@@ -1,19 +1,43 @@
 import unittest
 import SHMU_Kamery
 
-class Test_kameryZoznam(unittest.TestCase):
+class Test_Kamery(unittest.TestCase):
   def test_zoznamKamier(self):
     kamery = SHMU_Kamery.dajZoznamKamier()
     self.assertGreater(len(kamery), 0)
+    for kamera in kamery:
+      self.assertIsInstance(kamera, tuple)
+      self.assertEqual(len(kamera), 2)
+      self.assertIsInstance(kamera[0], str)
+      self.assertTrue(kamera[0].startswith('?page=1&id=webkamery&kamera=hdcam'))
+      self.assertIsInstance(kamera[1], str)
+      self.assertGreater(len(kamera[1]), 0)
 
-class Test_KameraParser(unittest.TestCase):
+  def test_kameraObrazky(self):
+    kamery = SHMU_Kamery.dajZoznamKamier()
+    obrazky = SHMU_Kamery.dajObrazkyKamery(kamery[0][0])
+    self.assertEqual(len(obrazky), 360)
+
+class Test_NastrojeParsera(unittest.TestCase):
   def test_dajAtribut(self):
     attrs = (('id', 'mainclass'),('href', 'http://link.address.com'),('class_name', 'test class'))
-    testParser = SHMU_Kamery.KameraParser()
-    self.assertEqual(testParser.dajAtribut(attrs, 'pokus'), None)
-    self.assertEqual(testParser.dajAtribut(attrs, 'href'), 'http://link.address.com')
-    self.assertEqual(testParser.dajAtribut(attrs, 'id'), 'mainclass')
-    self.assertEqual(testParser.dajAtribut(attrs, 'invalid'), None)
+    self.assertEqual(SHMU_Kamery.dajAtribut(attrs, 'pokus'), None)
+    self.assertEqual(SHMU_Kamery.dajAtribut(attrs, 'href'), 'http://link.address.com')
+    self.assertEqual(SHMU_Kamery.dajAtribut(attrs, 'id'), 'mainclass')
+    self.assertEqual(SHMU_Kamery.dajAtribut(attrs, 'invalid'), None)
+
+  def test_spracujObrazky(self):
+    parser = SHMU_Kamery.ObrazkyParser()
+    parser.vSkripte = True
+    obrazky = parser.handle_data('''  var img_dts = new Array(); var img_files = new Array();
+      img_dts[0]='07.02.2019 05:04 SEČ'; img_files[0]='20190207_050400.jpg';
+      img_dts[1]='07.02.2019 05:06 SEČ'; img_files[1]='20190207_050600.jpg';
+      img_dts[2]='07.02.2019 05:08 SEČ'; img_files[2]='20190207_050800.jpg';''')
+    self.assertEqual(len(parser.obrazky), 3)
+    self.assertEqual(parser.obrazky[0], ('20190207_050400.jpg', '07.02.2019 05:04 SEČ'))
+    self.assertEqual(parser.obrazky[1], ('20190207_050600.jpg', '07.02.2019 05:06 SEČ'))
+    self.assertEqual(parser.obrazky[2], ('20190207_050800.jpg', '07.02.2019 05:08 SEČ'))
+
 
 if __name__ == '__main__':
   unittest.main()
